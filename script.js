d5c9f107940a601efe0a460d3ab7dee0228e854d
Gabriel Henrique <work.gabriel12@gmail.com>
2024-08-14 14:30:30 -0300

    criando objeto com detalhes das despesas

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // Seleciona os elementos do formulário.
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
 // Captura o evento de input para formatar o valor.
 amount.oninput = () => {
@@ -9,7 +12,8 @@ amount.oninput = () => {
   // Transforma o valor em centavos. (ex: 150/100 = 1.50 que é equivalente a R$1,50)
   value = Number(value / 100)
 
-  amount.value = formatCurrencyBRL(value) //recebe o valor do input formatado.
+  // Atualiza o valor do input.
+  amount.value = formatCurrencyBRL(value)
 }
 
 function formatCurrencyBRL(value) {
@@ -17,6 +21,23 @@ function formatCurrencyBRL(value) {
     style: "currency",
     currency: "BRL",
   })
-  // Retorna o valor formatado
+  // Retorna o valor formatado.
   return value
+}
+
+// Captura o evento de submit do formulário para obter os valores.
+form.onsubmit = (event) => {
+  // Previne o comportamento de recarregar a página.
+  event.preventDefault()
+
+  // Cria um objeto com os detalhes na nova despesa.
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  }
+  console.log(newExpense)
 }
